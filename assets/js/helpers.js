c813ac0ffade655702673f4ed9ded14d3e3561ff
sofmediatec <vps.posomadrasah@gmail.com>
2026-06-14 03:19:43 +0800

    Update helpers.js

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -1,1 +1,33 @@
+function showLoading(){
 
+  const el =
+    document.getElementById(
+      'loadingOverlay'
+    );
+
+  if(el){
+
+    el.classList.remove(
+      'hidden'
+    );
+
+  }
+
+}
+
+function hideLoading(){
+
+  const el =
+    document.getElementById(
+      'loadingOverlay'
+    );
+
+  if(el){
+
+    el.classList.add(
+      'hidden'
+    );
+
+  }
+
+}
